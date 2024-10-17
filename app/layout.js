731ddd81848eb2df4bf4fa8551037210414e22df
Manav Mehta <manavmehta.official@gmail.com>
2024-10-18 02:31:01 +0530

    Update layout.js

--- app/layout.js
+++ app/layout.js
@@ -4,17 +4,6 @@ import { Container } from "@mui/material";
 import Navbar from "@/components/navbar/navbar";
 import Provider from "@/components/provider/provider";
 
-const geistSans = localFont({
-  src: "./fonts/GeistVF.woff",
-  variable: "--font-geist-sans",
-  weight: "100 900",
-});
-const geistMono = localFont({
-  src: "./fonts/GeistMonoVF.woff",
-  variable: "--font-geist-mono",
-  weight: "100 900",
-});
-
 export const metadata = {
   title: "finMinds",
   description: "Become Financialy Literate with finMinds",
@@ -24,7 +13,7 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en" suppressHydrationWarning>
       <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+        
       >
         <Provider>
           <Container maxWidth="xl">
